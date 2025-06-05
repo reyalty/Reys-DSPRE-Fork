3879ea579e3eee67b65885fc1c378c32c852144b
Corentin MACÉ <43670071+corentinmace@users.noreply.github.com>
2025-06-05 13:52:29 +0200

    scrollbar

--- DS_Map/Editors/BtxEditor.Designer.cs
+++ DS_Map/Editors/BtxEditor.Designer.cs
@@ -35,7 +35,9 @@
             this.exportImagePng = new System.Windows.Forms.Button();
             this.importImagePng = new System.Windows.Forms.Button();
             this.shinyCheckbox = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.overworldPictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -59,9 +61,9 @@
             // overworldPictureBox
             // 
             this.overworldPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.overworldPictureBox.Location = new System.Drawing.Point(153, 13);
+            this.overworldPictureBox.Location = new System.Drawing.Point(0, 0);
             this.overworldPictureBox.Name = "overworldPictureBox";
-            this.overworldPictureBox.Size = new System.Drawing.Size(49, 185);
+            this.overworldPictureBox.Size = new System.Drawing.Size(88, 209);
             this.overworldPictureBox.TabIndex = 2;
             this.overworldPictureBox.TabStop = false;
             // 
@@ -119,21 +121,34 @@
             this.shinyCheckbox.UseVisualStyleBackColor = true;
             this.shinyCheckbox.CheckedChanged += new System.EventHandler(this.shinyCheckbox_CheckedChanged);
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.overworldPictureBox);
+            this.panel1.Location = new System.Drawing.Point(143, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(88, 204);
+            this.panel1.TabIndex = 7;
+            // 
             // BtxEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(214, 209);
+            this.ClientSize = new System.Drawing.Size(232, 209);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.shinyCheckbox);
             this.Controls.Add(this.importImagePng);
             this.Controls.Add(this.exportImagePng);
             this.Controls.Add(this.showBtxFileButton);
-            this.Controls.Add(this.overworldPictureBox);
             this.Controls.Add(this.overworldList);
             this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(350, 248);
+            this.MinimumSize = new System.Drawing.Size(248, 248);
             this.Name = "BtxEditor";
-            this.Text = "BtxEditor";
+            this.Text = "Overworld (BTX) Editor";
             ((System.ComponentModel.ISupportInitialize)(this.overworldPictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,5 +163,6 @@
         private System.Windows.Forms.Button exportImagePng;
         private System.Windows.Forms.Button importImagePng;
         private System.Windows.Forms.CheckBox shinyCheckbox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
